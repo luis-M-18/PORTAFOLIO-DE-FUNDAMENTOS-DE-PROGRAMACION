5ca17619f3b13be83aba3678baff384630337081
luis-M-18 <99850629+luis-M-18@users.noreply.github.com>
2022-03-04 13:39:28 -0500

    Add files via upload

---
+++ OPERADORES BÁSICOS/resta_1.py
@@ -0,0 +1,6 @@
+#EJEMPLO DE RESTA
+x = 12
+y = 3
+result = 0
+result = (x - y)
+print("La resta es: ", result)
---
+++ OPERADORES BÁSICOS/resta_3.py
@@ -0,0 +1,8 @@
+
+num1 = int(input("INGRESE VALOR:"))
+num2 = int(input("INGRESE VALOR:"))
+
+
+resta = (num1 - num2)
+
+print("La resta es: ", resta)
