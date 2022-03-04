78b757d022e8ae03ffac79b80bcf43506d868474
luis-M-18 <99850629+luis-M-18@users.noreply.github.com>
2022-03-04 12:59:41 -0500

    Add files via upload

---
+++ OPERADORES BÁSICOS/op_aritméticos.py
@@ -0,0 +1,25 @@
+
+num1 = 10
+num2 = 7
+
+suma = num1 + num2
+print(num1, "+", num2, "=", suma)
+
+resta = num1 - num2
+print(num1, "-", num2, "=", resta)
+
+multiplicación = num1 * num2
+print(num1, "*", num2, "=", multiplicación)
+
+división = num1 / num2
+print(num1, "/", num2, "=", división)
+
+división_euclidiana = num1 // num2
+print(num1, "//", num2, "=", división_euclidiana)
+
+modulo = num1 % num2 
+print(num1, "%", num2, "=", modulo)
+
+potencia = num1 ** num2
+print(num1, "**", num2, "=", potencia)
+
